559aee437e3194a967310fe1c2c4e51019d9ac1e
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-01 06:13:26 -0400

    added users class to models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db # Import the db object from the app package
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone # Import the datetime and timezone classes from the datetime module
 
 
@@ -19,3 +20,16 @@ class Task(db.Model):
             'createdAt': self.createdAt, # The creation date of the task
             'dueDate': self.dueDate # The due date of the task
         }
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    date_created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
