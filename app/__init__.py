50982b411c248e99be01a9837ac32839a7818ab4
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-01 05:36:48 -0400

    Finished setting up the databse. Switched to Sqlite for simplicity

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy # Import the SQLAlchemy Class from the f
 from flask_migrate import Migrate # Import the Migrate Class from the flask_migrate module
 from config import Config # Import the Config class from the config module
 
+
 # Create an instance of Flask called app which will be the central object
 app = Flask(__name__)
 app.config.from_object(Config) # Set the database URI
@@ -12,4 +13,4 @@ db = SQLAlchemy(app) # Create an instance of the SQLAlchemy class called db
 migrate = Migrate(app, db) # Create an instance of the Migrate class called migrate
 
 # import the routes to the app
-from . import routes
+from . import routes, models # Import the routes and models modules from the app package
--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
 from . import db # Import the db object from the app package
+from datetime import datetime, timezone # Import the datetime and timezone classes from the datetime module
 
 
-
-class Task(db.model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True) # Create a column called id which is an integer and is the primary key
     title = db.Column(db.String, nullable=False) # Create a column called title which is a string and cannot be null
     description = db.Column(db.String, nullable=False) # Create a column called description which is a string and cannot be null
-    completed = db.Column(db.Boolean, default=False) # Create a column called completed which is a boolean and defaults to False
-    createdAt = db.Column(db.DateTime, default=datetime.utcnow) # Create a column called createdAt which is a datetime and defaults to the current time
+    completed = db.Column(db.Boolean, nullable=False, default=False) # Create a column called completed which is a boolean and cannot be null and defaults to False
+    createdAt = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc)) # Create a column called createdAt which is a datetime and cannot be null and defaults to the current time in UTC
     dueDate = db.Column(db.DateTime) # Create a column called dueDate which is a datetime and can be null
-    
