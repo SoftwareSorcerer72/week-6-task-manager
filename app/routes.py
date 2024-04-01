2622112dccd77e4358cd6452f90b8e15319d3ac3
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-01 04:40:00 -0400

    Created POST route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import request
+from flask import request, render_template
 from . import app
+from datetime import datetime
 from fake_data.tasks import tasks_list
+
 
 # Define a route
 @app.route("/")
@@ -18,7 +20,7 @@ def get_tasks():
     task = tasks_list 
     return task
 
-
+   
 # Get a Single Task By ID
 @app.route('/task/<int:task_id>')
 def get_task(task_id):
@@ -32,3 +34,34 @@ def get_task(task_id):
             return task
     # If we loop through all of the tasks without returning, the task with that ID does not exist
     return {'error': f"task with an ID of {task_id} does not exist"}, 404
+
+
+@app.route('/task', methods=['POST'])
+def create_task():
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    data = request.json
+    required_fields = ['title', 'description']
+    missing_fields =[]
+    for fields in required_fields:
+        if fields not in data:
+            missing_fields.append(fields)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in your request body"}, 400
+    
+    title = data['title']
+    description = data['description']
+
+    new_task = {
+        'id': len(tasks_list) + 1,
+        'title': title,
+        'description': description,
+        'completed': False,
+        'createdAt': datetime.now(datetime.UTC),
+        'dueDate': data.get('dueDate')
+    }
+
+
+
+
+    return data 
